88ccbc029e6980265093002d8fe504a1b030836b
Komarovski22 <komar.ivan@student.uzhnu.edu.ua>
2023-06-05 23:39:42 +0300

    Відалив логіку SharedWithMeAndDelete

--- server.js
+++ server.js
@@ -70,66 +70,6 @@ app.use(function (request, result, next) {
 });
 
 
-function removeSharedFileReturnUpdated (arr, _id){
-    for (var a = 0; a < arr.length;a++){
-        var file = (typeof arr[a].file === "undefined") ? arr[a] : arr[a].file;
-
-        if (file.type != "folder" && file._id == _id){
-            arr.splice(a, 1);
-            break;
-        }
-
-        if (file.type == "folder" && file.files.length > 0){
-            arr[a]._id = ObjectId(arr[a]._id);
-            removeSharedFileReturnUpdated(file.files, _id);
-        }
-    }
-    return arr;
-}
-
-function removeSharedFolderReturnUpdated (arr, _id){
-    for(var a = 0; a < arr.length; a++){
-        var file = (typeof arr[a].file === "undefined") ? arr[a] : arr[a].file;
-
-        if( file.type == "folder"){
-            if (file._id == _id){
-                arr.splice(a,1);
-                break;
-            }
-
-            if (file.files.length > 0){
-                file._id = ObjectId(file._id);
-                removeSharedFolderReturnUpdated(file.files, _id)
-            }
-        }
-    }
-    return arr;
-}
-
-
-    function recursiveGetSharedFolder (files, _id){
-        var singleFile = null;
-
-        for (var a = 0; a < files.length; a++){
-            var file = (typeof files[a].file === "undefined") ? files[a]:
-            files[a].file;
-
-            if (file.type == "folder") {
-                if (file._id == _id) {
-                return file;
-            }
-
-            if(file.files.length > 0){
-                singleFile = recursiveGetSharedFolder(file.files, _id);
-
-                if (singleFile != null){
-                    return singleFile;
-                }
-            }
-        }
-    }
-}
-
  function recursiveGetFile(files, _id){
     var singleFile = null;
 
@@ -249,6 +189,7 @@ http.listen(3000, function () {
             await DeleteObject.deleteFolder(database, request, result);
         });
 
+        // ShareViaLink - створення публічних ссилок і їх видалення.
         const ShareViaLink = require("./public/js/controller/ShareViaLink");
 
         app.post("/ShareViaLink", async function (request, result) {
@@ -296,6 +237,20 @@ http.listen(3000, function () {
             await DisplayListOfAllUsers.RemoveSharedAccess(database, request, result);
         });
         
+        // SharedWithMeAndDelete - показ файлів SharedWithMe/:_id та
+        const SharedWithMeAndDelete = require('./public/js/controller/SharedWithMeAndDelete');
+
+        app.get("/SharedWithMe/:_id?", async function (request, result) {
+            await SharedWithMeAndDelete.SharedWithMe(database, request, result);
+        });
+
+        app.post("/DeleteSharedDirectory", async function (request, result) {
+            await SharedWithMeAndDelete.DeleteSharedDirectory(database, request, result);
+        });
+
+        app.post("/DeleteSharedFile", async function (request, result) {
+            await SharedWithMeAndDelete.DeleteSharedFile(database, request, result);
+        });
         
 
 
@@ -350,106 +305,7 @@ http.listen(3000, function () {
             });
         });
 
-        app.post("/DeleteSharedFile", async function (request, result){
-            const _id = request.fields._id;
-
-            if (request.session.user){
-                var user = await database.collection("users").findOne({
-                    "_id": ObjectId(request.session.user._id)
-                });
-
-                var updatedArray = await removeSharedFileReturnUpdated(user.sharedWithMe, _id)
-                for (var a = 0; a < updatedArray.length; a++){
-                    updatedArray[a]._id = ObjectId(updatedArray[a]._id);
-                }
-                await database.collection("users").updateOne({
-                    "_id":ObjectId(request.session.user._id)
-                }, {
-                    $set:{
-                        "sharedWithMe":updatedArray
-                    }
-                });
-                const backURL = request.header('Referer') || '/';
-                result.redirect(backURL);
-                return false;
-            }
-            result.redirect("/Login");
-        });
-
-        app.post("/DeleteSharedDirectory", async function (request, result){
-            const _id = request.fields._id;
-
-            if(request.session.user){
-                var user = await database.collection("users").findOne({
-                    "_id": ObjectId(request.session.user._id)
-                });
-
-                var updatedArray = await 
-                removeSharedFolderReturnUpdated(user.sharedWithMe,_id)
-                for(var a = 0; a < updatedArray.length; a++){
-                    updatedArray[a]._id = ObjectId(updatedArray[a]._id);
-                }
-                await database.collection("users").updateOne({
-                    "_id":ObjectId(request.session.user._id)
-                },{
-                    $set:{
-                        "sharedWithMe":updatedArray
-                    }
-                });
-
-                const backURL = request.header('Referer') || '/';
-                result.redirect(backURL);
-                return false;
-            }
-            result.redirect("/Login");
-        });
-
-        app.get("/SharedWithMe/:_id?", async function (request, result){
-            const _id = request.params._id;
-            
-            if(request.session.user){
-                var user = await database.collection("users").findOne({
-                    "_id": ObjectId(request.session.user._id)
-                });
-
-                var files = null;
-                var folderName = "";
-                if (typeof _id == "undefined"){
-                    files = user.sharedWithMe;
-                } else {
-                    var folderObj = await recursiveGetSharedFolder(user.sharedWithMe, _id)
-
-                    if (folderObj == null){
-                        request.status = "error";
-                        request.message = "Folder not found.";
-                        result.render("Error",{
-                            "request":request
-                        });
-                        return false;
-                    }
-                    files = folderObj.files;
-                    folderName = folderObj.folderName;
-            }
-            if (files == null) {
-                request.status = "error";
-                request.message = "Directory not found.";
-                result.render("Error",{
-                    "request":request
-                });
-                return false;
-            }
-            result.render("SharedWithMe",{
-                "request":request,
-                "files":files,
-                "_id":_id,
-                "folderName":folderName
-            });
-            return false;
-        }
-        result.redirect("/Login")
-     });
-
-
+        
         // get all files shared with logged-in user
         app.get("/SharedWithMe/:_id?", async function (request, result) {
             result.render("SharedWithMe", {
